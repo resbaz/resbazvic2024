2962be4473efea32abe5f4425f239fa856a2030b
Shern Tee <shernren@gmail.com>
2023-10-25 14:44:51 +1000

    fixed numberbug in bash script

--- automation/scheduling/speaker-session-crosslist.sh
+++ automation/scheduling/speaker-session-crosslist.sh
@@ -3,4 +3,4 @@
 # generates list of Speaker Name:
 #  - Sessions spoken at (session-id, title and description)
 
-spklist=$(grep '^  id: ' speakers.yml | sed 's/  id: //'); for spk in ${spklist[@]}; do grep -A 2 '^  id: '"$spk"'$' speakers.yml | sed -e '/^  id:/ d; s/surname: //; s/name: //; s/"//g' | tr '\n' ' '; echo ""; grep -B 4 '^  speakers:.*'"$spk"'.*' sessions.yml | sed 's/^  id:/session-id:/'; echo ""; done
+spklist=$(grep '^  id: ' speakers.yml | sed 's/  id: //'); for spk in ${spklist[@]}; do grep -A 2 '^  id: '"$spk"'$' speakers.yml | sed -e '/^  id:/ d; s/surname: //; s/name: //; s/"//g' | tr '\n' ' '; echo ""; grep -B 4 '^  speakers:.*[[ ]'"$spk"'[],].*' sessions.yml | sed -e '/^  subtype:/ d; /^  speakers:/ d; s/^  id:/session-id:/'; echo ""; done
